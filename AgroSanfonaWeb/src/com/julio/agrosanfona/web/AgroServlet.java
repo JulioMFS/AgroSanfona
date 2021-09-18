0c5b17972e51109b993055b83252679872d80344
julio <sanfonaj@gmail.com>
2021-09-18 22:50:00 +0100

    ..

--- AgroSanfonaWeb/src/com/julio/agrosanfona/web/AgroServlet.java
+++ AgroSanfonaWeb/src/com/julio/agrosanfona/web/AgroServlet.java
@@ -29,7 +29,7 @@ public class AgroServlet extends HttpServlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
-//----------------------//
+//---------\---xx-----//
 	/**
 	 * @see Servlet#init(ServletConfig)
 	 */
